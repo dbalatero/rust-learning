0957fd9eee7ad8d23671d09be9e554b0b17a5e12
David Balatero <dbalatero@users.noreply.github.com>
2026-05-24 14:13:55 -0400

    loops

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -62,10 +62,18 @@
 //     }
 // }
 
-fn main() {
-    let a = [10, 20, 30, 40, 50];
+// fn main() {
+//     let a = [10, 20, 30, 40, 50];
+//
+//     for element in a {
+//         println!("the value is {element}");
+//     }
+// }
 
-    for element in a {
-        println!("the value is {element}");
+fn main() {
+    for number in (1..4).rev() {
+        println!("{number}!");
     }
+
+    println!("LIFTOFF!");
 }
