7cf6ff97180ffe45cae85b5cd14cdf7c767d2f9b
David Balatero <dbalatero@users.noreply.github.com>
2026-06-15 09:08:49 -0400

    structs pt1

--- structs/src/bin/user.rs
+++ structs/src/bin/user.rs
@@ -15,8 +15,15 @@ fn build_user(email: String, username: String) -> User {
 }
 
 fn main() {
-    println!("Hello");
-
     let mut user1 = build_user(String::from("db@example.com"), String::from("db"));
     user1.email = String::from("db@new.com");
+
+    println!("Hello {0}", user1.email);
+
+    let user2 = User {
+        email: String::from("googoo@gaga.com"),
+        ..user1
+    };
+
+    println!("Also hi to {0}", user2.email);
 }
